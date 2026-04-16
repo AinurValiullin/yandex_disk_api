48ecf5ae00b8531ec3d5f1f2d529e857e8cef53f
Ainur Valiullin <aiva100392@yandex.ru>
2026-04-16 09:19:11 +0500

    v2

--- README.md
+++ README.md
@@ -34,12 +34,25 @@ pip install -r requirements.txt
 1. В корне проекта (в папке yandex_disk_api) создайте файл .env
 2. Добавьте в него:
 ```python
-TOKEN=твой_токен 
+TOKEN=твой_токен #вместо твой_токен необходимо ввести OAuth-токен Яндекс Диска
 ```
 
+![env example](images/env_example.png)
+
 ## Запуск тестов
+
+Перейдите в корневую папку проекта:
+
+```bash
+cd yandex_disk_api
+```
+
+Запустите тесты:
 
 ```bash
 python -m pytest -v
 ```
 
+## Результат тестов
+
+![test result](images/test_result.png)
--- tests/test_get_resources.py
+++ tests/test_get_resources.py
@@ -10,25 +10,25 @@ def test_get_files():
     response = requests.get(url=RESOURCES_URL, headers=get_headers(), params=params)
     assert response.status_code == 200 #проверка что код в ответе 200
 
-    data = response.json()
-    assert isinstance(data, dict)
+    data = response.json() #сохраняем в переменную data json ответа
+    assert isinstance(data, dict) #проверяем что ответ соответствует структуре ключ-значение (JSON)
     assert "_embedded" in data #проверка что в ответе есть embedded
     assert "items" in data["_embedded"] #проверка что есть итемс в ответе в поле embedded
-    items = data["_embedded"]["items"]
+    items = data["_embedded"]["items"] #в переменную items сохраняем список файлов/папок из ответа
     assert isinstance(items, list) #проверка что итемс это список
     
-    assert "error" not in data
+    assert "error" not in data #проверяем что в JSON ответе нет ключа "error"
 
-    for item in items:
-        assert "name" in item
-        assert "type" in item
-        assert item["type"] in ["file", "dir"]
-        assert "path" in item
-        assert "created" in item
-        assert "modified" in item
-        assert "resource_id" in item
-        assert "revision" in item
-        assert "comment_ids" in item
-        assert isinstance(item["name"], str)
-        assert isinstance(item["type"], str)
-        assert isinstance(item["path"], str)
+    for item in items: #проходимся по элементам списка
+        assert "name" in item #проверка обязательныъ полей в ответе
+        assert "type" in item #проверка обязательныъ полей в ответе
+        assert item["type"] in ["file", "dir"] #проверка что корректный тип значения 
+        assert "path" in item #проверка обязательныъ полей в ответе
+        assert "created" in item #проверка обязательныъ полей в ответе
+        assert "modified" in item #проверка обязательныъ полей в ответе
+        assert "resource_id" in item #проверка обязательныъ полей в ответе
+        assert "revision" in item #проверка обязательныъ полей в ответе
+        assert "comment_ids" in item #проверка обязательныъ полей в ответе
+        assert isinstance(item["name"], str) #проверка что корректный тип значения
+        assert isinstance(item["type"], str) #проверка что корректный тип значения
+        assert isinstance(item["path"], str) #проверка что корректный тип значения
